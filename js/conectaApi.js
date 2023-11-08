9d151a7081a5f3a51ea16f6d767e54aca4d984a3
Jessica Lorenzon <jessica_lorenzon@hotmail.com>
2023-11-07 23:10:43 -0300

    implementando funcionalidades

---
+++ js/conectaApi.js
@@ -0,0 +1,38 @@
+async function listaVideos() {
+    const conexao = await fetch('http://localhost:3000/videos');// sem declarar nada, considera method get
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch('http://localhost:3000/videos', {
+        method: "POST", // usando o method post, enviar dados pra api
+        headers: {
+            "Content-type": "application/json" // informando qual tipo de arquivo e informação irá enviar
+        },
+        body: JSON.stringify({ // colocando quais informações serão enviadas e o stringfy coloca tudo que estamos enviando como string
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaApi = {
+    listaVideos,
+    criaVideo,
+    buscaVideo
+}
---
+++ js/criarVideo.js
@@ -0,0 +1,20 @@
+import { conectaApi } from "./conectaApi.js";
+
+const formulario = document.querySelector('[data-formulario]');
+
+async function criarVideo(evento) {
+    evento.preventDefault(); 
+    // o evento submit tem como padrão dar reload na pagina após enviar informação, colocamos preventDefault() para tirar o reload da pagina após o envio
+
+    const imagem = document.querySelector('[data-imagem]').value;
+    const url = document.querySelector('[data-url]').value;
+    const titulo = document.querySelector('[data-titulo]').value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+
+    // redirecionando o usuário para outra página após ter enviado os dados com sucesso
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+formulario.addEventListener('submit', evento => criarVideo(evento));
